71f8f599f96e704dd1c36d943aa0cc7aeb91d910
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-09-02 21:30:45 +0100

    config backend and create schemes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,45 @@
-import '../styles/globals.css'
+//NEXT IMPORTS
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+//REACT IMPORTS
+import { useState, useEffect } from 'react'
+//GOOGLE LOGIN
+import { GoogleLogin, GoogleLogout } from 'react-google-login'
+//CSS IMPORTS
+import '../styles/globals.css'
+//REACT-ICONS
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io'
+//COMPONENT IMPORTS
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setisSSR] = useState(true)
+
+  useEffect(() => {
+   setisSSR(false);
+  }, []);
+  
+  if(isSSR) return null;
+
+  return (
+    <div>
+      <Navbar/>
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:overflow-auto'>
+         <Sidebar/>
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+         <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+
+  ) 
 }
 
 export default MyApp
