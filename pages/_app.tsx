d652622852715a50967d97e4959eb458966d6bed
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-10-11 13:35:10 +0100

    finish log in and log out, start upload page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,16 @@
-//NEXT IMPORTS
 import type { AppProps } from "next/app";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-//REACT IMPORTS
-import { useState, useEffect } from "react";
-//GOOGLE LOGIN
-import { GoogleOAuthProvider, useGoogleOneTapLogin } from "@react-oauth/google";
-//CSS IMPORTS
-import "../styles/globals.css";
-//REACT-ICONS
-import { AiOutlineLogout } from "react-icons/ai";
-import { BiSearch } from "react-icons/bi";
-import { IoMdAdd } from "react-icons/io";
-//COMPONENT IMPORTS
+import { useEffect, useState } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [isSSR, setisSSR] = useState(true);
+  const [isSSR, setIsSSR] = useState(true);
 
   useEffect(() => {
-    setisSSR(false);
+    setIsSSR(false);
   }, []);
 
   if (isSSR) return null;
@@ -30,13 +19,15 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <GoogleOAuthProvider
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
     >
-      <Navbar />
-      <div className="flex gap-6 md:gap-20">
-        <div className="h-[92vh] overflow-hidden xl:overflow-auto">
-          <Sidebar />
-        </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-          <Component {...pageProps} />
+      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+        <Navbar />
+        <div className="flex gap-6 md:gap-20 ">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </GoogleOAuthProvider>
